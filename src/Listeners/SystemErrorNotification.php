afe229bcfc8fae21e78483e3a9831ca9d6d839d6
andersonfantw <47234075+andersonfantw@users.noreply.github.com>
2021-04-06 16:21:09 +0800

    Update SystemErrorNotification.php

--- src/Listeners/SystemErrorNotification.php
+++ src/Listeners/SystemErrorNotification.php
@@ -27,6 +27,6 @@ class SystemErrorNotification
      */
     public function handle(SystemErrorEvent $event)
     {
-        Log::error(springf('[%s][%s]%s - %s. %s',datetime('Y-m-d H:i:s'), $event->sender, $event->subject, $event->content, print_r($event->debug)));
+        Log::error(sprintf('[%s][%s]%s - %s. %s',datetime('Y-m-d H:i:s'), $event->sender, $event->subject, $event->content, print_r($event->debug)));
     }
 }
