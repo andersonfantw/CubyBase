d6e0e2681664012d32a255d3f73f6a67598e2b7e
andersonfantw <47234075+andersonfantw@users.noreply.github.com>
2021-04-06 16:19:56 +0800

    Update SystemNoticeNotification.php

--- src/Listeners/SystemNoticeNotification.php
+++ src/Listeners/SystemNoticeNotification.php
@@ -27,6 +27,6 @@ class SystemNoticeNotification
      */
     public function handle(SystemNoticeEvent $event)
     {
-        Log::notice(springf('[%s][%s]%s - %s. %s',datetime('Y-m-d H:i:s'), $event->sender, $event->subject, $event->content, print_r($event->debug)));
+        Log::notice(sprintf('[%s][%s]%s - %s. %s',datetime('Y-m-d H:i:s'), $event->sender, $event->subject, $event->content, print_r($event->debug)));
     }
 }
