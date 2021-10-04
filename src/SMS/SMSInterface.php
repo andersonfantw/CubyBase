b9508824c5d0168402f038bc1f21f7596abb23a4
ANDERSONFAN-TPC\AndersonFan <andersonfan@chinayss.hk>
2021-10-04 11:28:27 +0800

    fix

--- src/SMS/SMSInterface.php
+++ src/SMS/SMSInterface.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 Interface SMSInterface
 {
     // 發送簡訊。要設定短訊語言，輸入的內容依會驗證內容長度。
-    public function send(): array;
+    // public function send(Mixin $message): array;
     // 與簡訊主機通訊
     public function queryServer(String $url_name);
     // 解析回傳結果，若是錯誤訊息做相應的通知
